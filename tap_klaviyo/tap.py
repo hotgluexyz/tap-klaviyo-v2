60ee5115ea6ac47c4e47b03bd86d49fc7feb9c3b
LiorB-D <liorbd@outlook.com>
2025-10-09 18:00:58 -0400

    Support default reports

--- tap_klaviyo/tap.py
+++ tap_klaviyo/tap.py
@@ -74,6 +74,11 @@ class TapKlaviyo(Tap):
         ),
     ).to_dict()
 
+    def metric_name_to_id(self, metrics, metric_name):
+        metric_id = next((metric["id"] for metric in metrics if metric["attributes"]["name"] == metric_name), None)
+        return metric_id
+
+
     def discover_streams(self) -> List[Stream]:
         """Return a list of discovered streams."""
         discovered_streams = []
@@ -87,6 +92,7 @@ class TapKlaviyo(Tap):
         # fetch all metrics (unless they are missing from the existing catalog)
         is_sync = self.input_catalog is not None
         should_query_metrics = next((t for t in iter(self.input_catalog.keys()) if "events_" in t), None) if is_sync else True
+        metrics = None
         if should_query_metrics:
             try:
                 metrics_stream = MetricsStream(tap=self)
@@ -111,18 +117,77 @@ class TapKlaviyo(Tap):
                 )(tap=self)
                 discovered_streams.append(event_stream)
 
-        # Add custom reports streams from config
-        custom_reports = self.config.get("custom_reports", [])
-        for report_config in custom_reports:
-            try:
+
+
+
+        if metrics:
+            default_reports = self._get_default_reports(metrics)
+            custom_reports = self.config.get("custom_reports", [])
+
+            for report_config in default_reports:
+                try:
+                    report_stream = ReportStream(tap=self, report_config=report_config)
+                    report_stream.replication_key = "date"
+                    report_stream.primary_keys = ["date", "metric_id"] + report_stream.dimensions
+                    discovered_streams.append(report_stream)
+                except Exception as e:
+                    self.logger.error(f"Error creating default report stream {report_config['name']}: {e}")
+
+            # Add custom reports streams from config
+            custom_reports = self.config.get("custom_reports", [])
+            for report_config in custom_reports:
+                if report_config.get("metric_name"):
+                    metric_id = self.metric_name_to_id(metrics, report_config["metric_name"])
+                    if not metric_id:
+                        raise ValueError(f"Metric name {report_config['metric_name']} not found")
+                    report_config["metric_id"] = metric_id
+
                 report_stream = ReportStream(tap=self, report_config=report_config)
                 report_stream.replication_key = "date"
                 report_stream.primary_keys = ["date", "metric_id"] + report_stream.dimensions
                 discovered_streams.append(report_stream)
-            except Exception as e:
-                self.logger.error(f"Error creating custom report stream {report_config['name']}: {e}")
 
         return discovered_streams
+
+    def _get_default_reports(self, metrics):
+            """Return default report configurations."""
+            return [
+                {
+                    "name": "emails_opened_per_day", 
+                    "metric_id": self.metric_name_to_id(metrics, "Opened Email"),
+                    "dimensions": "Campaign Name,$message",
+                    "aggregation_types": "count",
+                    "interval": "day"
+                },
+                {
+                    "name": "emails_clicked_per_day",
+                    "metric_id": self.metric_name_to_id(metrics, "Clicked Email"),  # Clicked Email
+                    "dimensions": "Campaign Name,$message",
+                    "aggregation_types": "count",
+                    "interval": "day"
+                },
+                {
+                    "name": "emails_bounced_per_day",
+                    "metric_id": self.metric_name_to_id(metrics, "Bounced Email"),  # Bounced Email
+                    "dimensions": "Campaign Name,$message",
+                    "aggregation_types": "count",
+                    "interval": "day"
+                },
+                {
+                    "name": "emails_received_per_day",
+                    "metric_id": self.metric_name_to_id(metrics, "Received Email"),  # Received Email
+                    "dimensions": "Campaign Name,$message",
+                    "aggregation_types": "count",
+                    "interval": "day"
+                },
+                {
+                    "name": "campaign_performance_daily",
+                    "metric_id": self.metric_name_to_id(metrics, "Opened Email"),  # Opened Email
+                    "dimensions": "Campaign Name,$message",
+                    "aggregation_types": "count",
+                    "interval": "day"
+                }
+            ]
 
 if __name__ == "__main__":
     TapKlaviyo.cli()
